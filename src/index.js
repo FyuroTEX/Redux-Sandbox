23f376bb93852769bd8148b81b798deeff92d8ce
FyuroTEX <yurilev4@gmail.com>
2020-02-28 18:30:52 +0200

    create store and dispatch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 
 const reducer = (state = 0, action) => {
 
@@ -9,8 +11,11 @@ const reducer = (state = 0, action) => {
     };
 
 };
-let state = reducer(undefined, {});
-state = reducer(state, { type: 'INC' });
-console.log(state);
-state = reducer(state, { type: 'INC' });
-console.log(state);
+
+const store = createStore(reducer);
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch({ type: 'INC' });
+store.dispatch({ type: 'INC' });
