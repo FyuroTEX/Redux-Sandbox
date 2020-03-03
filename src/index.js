ed716dcce14517f4a230403eef85b398adaf7250
FyuroTEX <yurilev4@gmail.com>
2020-03-03 03:05:59 +0200

    random button and add parameters

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'RND':
+            return state + action.payload;
         case 'DEC':
             return state - 1;
         default:
@@ -20,6 +22,10 @@ document.getElementById('inc').addEventListener('click', () => {
 });
 document.getElementById('dec').addEventListener('click', () => {
     store.dispatch({ type: 'DEC' });
+});
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({ type: 'RND', payload });
 });
 const update = () => {
     document.getElementById('counter').innerHTML = store.getState();
