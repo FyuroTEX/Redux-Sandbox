4d2bfc7328ba62966add54ddca6b5ec297dea8b0
FyuroTEX <yurilev4@gmail.com>
2020-03-04 02:43:16 +0200

    project structure

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import { createStore } from 'redux';
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 
-const reducer = (state = 0, action) => {
-
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'RND':
-            return state + action.payload;
-        case 'DEC':
-            return state - 1;
-        default:
-            return state;
-    };
-
-};
 
 const store = createStore(reducer);
 
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (payload) => ({ type: 'RND', payload });
 
 document.getElementById('inc').addEventListener('click', () => {
     store.dispatch(inc());
