8ba0d0c04bd1f5e51903ce112192d96cb85c744b
FyuroTEX <yurilev4@gmail.com>
2020-03-05 14:45:40 +0200

    pre bindActionCreators

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import { inc, dec, rnd } from './actions';
 
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
+const bindActionCreator = (creator, dispatch) => (...args) => {
+    dispatch(creator(...args));
+};
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
+const incDispatch = bindActionCreator(inc,dispatch);
+// const decDispatch = () => dispatch(dec());
+const rndDispatch = bindActionCreator(rnd, dispatch);   // const rndDispatch = (payload) => dispatch(rnd(payload));
+
+document.getElementById('inc').addEventListener('click', incDispatch);
+
 document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
+    dispatch(dec());
 });
 document.getElementById('rnd').addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(payload));
+    rndDispatch(payload)
 });
+
 const update = () => {
     document.getElementById('counter').innerHTML = store.getState();
 };
--- src/reducer.js
+++ src/reducer.js
@@ -3,10 +3,10 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
-        case 'RND':
-            return state + action.payload;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state + action.payload;
         default:
             return state;
     };
