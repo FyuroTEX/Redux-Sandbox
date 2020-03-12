6dfd88200b18dc0ccc5d78be7ed35d7673c063b3
FyuroTEX <yurilev4@gmail.com>
2020-03-12 03:46:21 +0200

    change actions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,16 +7,17 @@ const Counter = ({ counter, inc, dec, rnd }) => {
     return (
         <div className='jumbotron'>
             <h2 id="counter">{counter}</h2>
-            <button id="inc"
-                class="btn btn-primary btn-lg"
-                onClick={inc}>
-                INC
-                </button>
             <button id="dec"
                 class="btn btn-primary btn-lg"
                 onClick={dec}>
-                DEC
+                -DEC
                 </button>
+            <button id="inc"
+                class="btn btn-primary btn-lg"
+                onClick={inc}>
+                INC+
+                </button>
+
             <button id="rnd"
                 class="btn btn-primary btn-lg"
                 onClick={rnd}>
@@ -32,14 +33,11 @@ const mapStateToProps = (state) => {
 };
 const mapDispatchToProps = (dispatch) => {
 
-    const { inc,dec,rnd}=bindActionCreators(actions, dispatch);
+    const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
     return {
-        inc: () => dispatch(inc()),
-        dec: () => dispatch(dec()),
-        rnd: () => {
-            const value = Math.floor(Math.random() * 10);
-            dispatch(rnd(value));
-        }
+        inc,
+        dec,
+        rnd
     };
 };
 
