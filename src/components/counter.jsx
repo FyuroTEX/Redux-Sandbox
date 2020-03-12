c563d599c8af211e5393e42a34badeafa30886b2
FyuroTEX <yurilev4@gmail.com>
2020-03-12 02:20:03 +0200

    connect(), mapStateToProps()

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Counter = ({ counter, inc, dec, rnd }) => {
     return (
@@ -6,17 +7,27 @@ const Counter = ({ counter, inc, dec, rnd }) => {
             <h2 id="counter">{counter}</h2>
             <button id="inc"
                 class="btn btn-primary btn-lg"
-                onClick={inc}
-            >INC</button>
+                onClick={inc}>
+                INC
+                </button>
             <button id="dec"
                 class="btn btn-primary btn-lg"
-                onClick={dec}
-            >DEC</button>
+                onClick={dec}>
+                DEC
+                </button>
             <button id="rnd"
                 class="btn btn-primary btn-lg"
-                onClick={rnd}
-            >RND</button>
+                onClick={rnd}>
+                RND
+                </button>
         </div>
     );
 };
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+
+};
+
+export default connect(mapStateToProps)(Counter);
